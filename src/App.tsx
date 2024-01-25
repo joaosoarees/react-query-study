75fb8a99f81844225b78f11312152d63d79753bf
Joao Soares <jsoareez@gmail.com>
2024-01-24 23:12:34 -0300

    feat: minimum imp to use react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { QueryClient, useQuery } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
 export function App() {
-  return <h1 className="text-white">Oskei</h1>;
+  const { data } = useQuery(
+    {
+      queryKey: ['users'],
+      queryFn: async (): Promise<IUser[]> => {
+        const response = await fetch('http://localhost:3000/users');
+        return response.json();
+      },
+    },
+    queryClient,
+  );
+
+  return (
+    <div>
+      {data?.map((user) => (
+        <div key={user.id}>
+          <strong className="block">{user.name}</strong>
+          <small>{user.email}</small>
+        </div>
+      ))}
+    </div>
+  );
 }
